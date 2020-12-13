f79a774761e3bf83207d0868cc74bf2475c083c1
Andre De Jager <andrejohannesdejager@gmail.com>
2020-12-13 19:44:24 +0200

    added corspolicy

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,13 @@ namespace API
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddAutoMapper(typeof(MappingProfiles));
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
             
         }
 
@@ -45,6 +52,7 @@ namespace API
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
 
